8fe56b1df610b2d883f0f51d0ff2383f2cea9ba3
Yurii Dudok <yurii.dudok@globallogic.com>
2021-11-02 12:22:49 +0200

    First commit

---
+++ dataTypesAndVariables.js
@@ -0,0 +1,39 @@
+// const firstName = "Yurii";
+// const secondName = "Dudok";
+// let age = 23;
+
+// console.log(firstName, secondName, age);
+
+
+// let Cars = ["BMW", "Audi", "Hundai", "Honda"];
+// console.log(typeof Cars);
+// Cars [2] = "Citroen";
+// console.log(Cars[2]);
+
+// let productCategoryID = {
+// smartphones : 001,
+// goodsForHome: 002,
+// clothes: 003,
+// pcAndLaptops: 004,
+// userCanBuyGoods: true,
+
+// };
+
+// console.log(typeof productCategoryID.userCanBuyGoods, typeof productCategoryID['clothes']);
+
+// let a;
+// a = 10;
+// const informMessage = "Goods on website remaining: ";
+// console.log(informMessage + a);
+
+// let a = "6";
+// let b = 6;
+// console.log(a===b); 
+
+// let a = 10;
+// let b = "10";
+// console.log(typeof(a.toString() === b)); // boolean, тому що прирівнюємоємо
+
+// let a = 10;
+// let b = "10";
+// console.log(a == b); // true, тому шо перевірили лише по значенню, а не також за типом данних
