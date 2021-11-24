bf05628f23841a9c1ee745b7d74c670888be312c
Yurii Dudok <vovchuk1998@gmail.com>
2021-11-24 10:46:55 +0200

    Classes

---
+++ ooplnJS.js
@@ -0,0 +1,11 @@
+class Devices {
+    constructor(characteristics) {
+        this.companyName = characteristics.companyName;
+        this.hasKeyBoard = characteristics.hasKeyBoard;
+        this.releaseYear = characteristics.releaseYear;
+    }
+}
+
+ {
+
+}
